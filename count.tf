096be77f2a0762bd27f589df2ca42f8097f67cfa
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-21 22:25:07 +0530

    Create count.tf

---
+++ count.tf
@@ -0,0 +1,45 @@
+variable "project" {}
+
+variable "region" {
+    default =    "us-central1"
+}
+
+variable "zone"{
+    default = "us-central1-b"
+}
+variable "machine_type"{
+    default = "n1-standard-1"
+}
+variable "name"{
+    default = "myterraform-from-module"
+}
+variable "image" {
+    default = "projects/debian-cloud/global/images/debian-12-bookworm-v20260210"
+}
+variable "network" {
+  type     = string
+  default  = "default"
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+  project      = var.project
+  count        = 3
+
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+  
+# Define the network interface using the "default" network
+  network_interface {
+    network = var.network
+    # Assign an ephemeral public IP
+    access_config {
+    }
+  }
+}
